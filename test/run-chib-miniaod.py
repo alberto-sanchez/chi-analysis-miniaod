a414f22f595860eab06fb3cb7923b293909cc805
Alberto Sanchez Hernandez <alberto.sanchez@mail.com>
2017-08-02 13:19:32 -0500

    config for chib

---
+++ test/run-chib-miniaod.py
@@ -0,0 +1,120 @@
+input_filename = '/store/data/Run2017B/MuOnia/MINIAOD/PromptReco-v1/000/297/723/00000/9040368C-DE5E-E711-ACFF-02163E0134FF.root'
+ouput_filename = 'rootuple.root'
+
+import FWCore.ParameterSet.Config as cms
+process = cms.Process("Rootuple")
+
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_38T_cff')
+process.load('Configuration.StandardSequences.Reconstruction_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_condDBv2_cff')
+from Configuration.AlCa.GlobalTag_condDBv2 import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '92X_dataRun2_Prompt_v4', '')
+
+process.load("FWCore.MessageLogger.MessageLogger_cfi")
+process.MessageLogger.cerr.FwkReport.reportEvery = 20000
+
+process.maxEvents = cms.untracked.PSet(input = cms.untracked.int32(-1))
+process.source = cms.Source("PoolSource",fileNames = cms.untracked.vstring(input_filename))
+process.TFileService = cms.Service("TFileService",fileName = cms.string(ouput_filename))
+process.options   = cms.untracked.PSet( wantSummary = cms.untracked.bool(False))
+
+# In MiniAOD, the PATMuons are already present. We just need to run Onia2MuMu, with a selection of muons.
+process.oniaSelectedMuons = cms.EDFilter('PATMuonSelector',
+   src = cms.InputTag('slimmedMuons'),
+   cut = cms.string('muonID(\"TMOneStationTight\")'
+                    ' && abs(innerTrack.dxy) < 0.3'
+                    ' && abs(innerTrack.dz)  < 20.'
+                    ' && innerTrack.hitPattern.trackerLayersWithMeasurement > 5'
+                    ' && innerTrack.hitPattern.pixelLayersWithMeasurement > 0'
+                    ' && innerTrack.quality(\"highPurity\")'
+                    ' && (abs(eta) <= 2.4 && pt > 3.5)'
+   ),
+   filter = cms.bool(True)
+)
+
+process.load("HeavyFlavorAnalysis.Onia2MuMu.onia2MuMuPAT_cfi")
+process.onia2MuMuPAT.muons=cms.InputTag('oniaSelectedMuons')
+process.onia2MuMuPAT.primaryVertexTag=cms.InputTag('offlineSlimmedPrimaryVertices')
+process.onia2MuMuPAT.beamSpotTag=cms.InputTag('offlineBeamSpot')
+process.onia2MuMuPAT.dimuonSelection=cms.string("0.2 < mass && abs(daughter('muon1').innerTrack.dz - daughter('muon2').innerTrack.dz) < 25")
+process.onia2MuMuPAT.addMCTruth = cms.bool(False)
+
+process.triggerSelection = cms.EDFilter("TriggerResultsFilter",
+                                        triggerConditions = cms.vstring('HLT_Dimuon10_Upsilon_Barrel_Seagulls_v*',
+                                                                        'HLT_Dimuon12_Upsilon_eta1p5_v*'
+                                                                       ),
+                                        hltResults = cms.InputTag( "TriggerResults", "", "HLT" ),
+                                        l1tResults = cms.InputTag( "" ),
+                                        throw = cms.bool(False)
+                                        )
+
+process.Onia2MuMuFiltered = cms.EDProducer('DiMuonFilter',
+      OniaTag             = cms.InputTag("onia2MuMuPAT"),
+      singlemuonSelection = cms.string(""),
+      dimuonSelection     = cms.string("8.5 < mass && mass < 11.5 && pt > 10. && abs(y) < 1.2 && charge==0 && userFloat('vProb') > 0.01"),
+      do_trigger_match    = cms.bool(False),    #disabled temporary
+      HLTFilters          = cms.vstring(
+                'hltDisplacedmumuFilterDimuon10UpsilonBarrelnoCow',
+                'hltDisplacedmumuFilterDimuon12Upsilons'
+                          ),
+)
+
+process.DiMuonCounter = cms.EDFilter('CandViewCountFilter',
+    src       = cms.InputTag("Onia2MuMuFiltered"),
+    minNumber = cms.uint32(1),
+    filter    = cms.bool(True)
+)
+
+process.chiProducer = cms.EDProducer('OniaPhotonProducer',
+    conversions     = cms.InputTag("oniaPhotonCandidates","conversions"),
+    dimuons         = cms.InputTag("Onia2MuMuFiltered"),
+    pi0OnlineSwitch = cms.bool(False),
+    deltaMass       = cms.vdouble(0.0,2.0),
+    dzmax           = cms.double(0.5),
+    triggerMatch    = cms.bool(False)  # trigger match is performed in Onia2MuMuFiltered
+)
+
+process.chiFitter1S = cms.EDProducer('OniaPhotonKinematicFit',
+                          chi_cand = cms.InputTag("chiProducer"),
+                          upsilon_mass = cms.double(9.46030), # GeV   1S = 9.46030   2S = 10.02326    3S = 10.35520  J/psi=3.0969
+                          product_name = cms.string("y1S")
+                         )
+
+process.chiFitter2S = cms.EDProducer('OniaPhotonKinematicFit',
+                          chi_cand = cms.InputTag("chiProducer"),
+                          upsilon_mass = cms.double(10.02326), # GeV   1S = 9.46030   2S = 10.02326    3S = 10.35520  J/psi=3.0969
+                          product_name = cms.string("y2S")
+                         )
+
+process.chiFitter3S = cms.EDProducer('OniaPhotonKinematicFit',
+                          chi_cand = cms.InputTag("chiProducer"),
+                          upsilon_mass = cms.double(10.35520), # GeV   1S = 9.46030   2S = 10.02326    3S = 10.35520  J/psi=3.0969
+                          product_name = cms.string("y3S")
+                         )
+
+
+process.chiSequence = cms.Sequence(
+                                   process.triggerSelection *
+				   process.oniaSelectedMuons *
+				   process.onia2MuMuPAT *
+				   process.Onia2MuMuFiltered *
+		                   process.DiMuonCounter *
+				   process.chiProducer *
+				   process.chiFitter1S *
+                                   process.chiFitter2S *
+                                   process.chiFitter3S
+				   )
+
+process.rootuple = cms.EDAnalyzer('chibRootupler',
+                          chi_cand = cms.InputTag("chiProducer"),
+			  ups_cand = cms.InputTag("Onia2MuMuFiltered"),
+                          refit1S  = cms.InputTag("chiFitter1S","y1S"),
+			  refit2S  = cms.InputTag("chiFitter2S","y2S"),
+			  refit3S  = cms.InputTag("chiFitter3S","y3S"),
+                          primaryVertices = cms.InputTag("offlineSlimmedPrimaryVertices"),
+                          TriggerResults  = cms.InputTag("TriggerResults", "", "HLT"),
+                          isMC = cms.bool(False)
+                         )
+
+process.p = cms.Path(process.chiSequence*process.rootuple)
